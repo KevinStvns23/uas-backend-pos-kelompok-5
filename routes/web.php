c113f6c467c6e010e309ea53740011c40e2274b2
Sept Youlan <sept.youlan99@gmail.com>
2026-05-28 05:08:58 +0700

    feat(subkategori): melengkapi fungsi repository, service, dan controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubCategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('sub-categories', SubCategoryController::class);
