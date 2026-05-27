a9887e35468fd48126c656ff6ecacdc8c6fd6b8e
Ransley77 <familyrbbwand77@gmail.com>
2026-05-27 23:58:46 +0700

    Menambahkan migrate users

--- KasirFotoCopy-project/database/migrations/0001_01_01_000000_create_users_table.php
+++ KasirFotoCopy-project/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,14 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+    $table->id();
+    $table->string('username')->unique();
+    $table->string('password');
+    $table->string('nama_lengkap');
+    $table->string('alamat');
+    $table->string('no_telp');
+    $table->string('shift')->nullable();
+    $table->string('status')->default('aktif');
+    $table->timestamps();
+    });
+        
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
