f5e3be2fa08f39fc9088ddb0df0b48816bd032e5
Sept Youlan <sept.youlan99@gmail.com>
2026-05-28 18:24:16 +0700

    feat(diskon): melengkapi logic crud pada repository, service, dan controller

--- KasirFotoCopy-project/database/migrations/2026_05_28_063737_create_discounts_table.php
+++ KasirFotoCopy-project/database/migrations/2026_05_28_063737_create_discounts_table.php
@@ -6,19 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('sub_categories', function (Blueprint $table) {
+        Schema::create('discounts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->string('name');
-            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('sub_categories');
+        Schema::dropIfExists('discounts');
     }
 };
