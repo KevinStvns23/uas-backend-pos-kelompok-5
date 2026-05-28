146fee27e0609ac628b38f99a1f8d2db8ce72337
Ransley77 <familyrbbwand77@gmail.com>
2026-05-28 18:55:19 +0700

    Menambahkan Usercontroller ke dalam routes

--- KasirFotoCopy-project/routes/web.php
+++ KasirFotoCopy-project/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('users', UserController::class);
