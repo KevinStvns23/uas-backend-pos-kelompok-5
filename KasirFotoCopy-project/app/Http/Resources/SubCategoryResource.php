7267badf6b2ad8bce28a832738be38043d344537
Sept Youlan <sept.youlan99@gmail.com>
2026-06-01 22:16:26 +0700

    feat(diskon): membuat api resource untuk memformat respon json diskon

--- KasirFotoCopy-project/app/Http/Resources/SubCategoryResource.php
+++ KasirFotoCopy-project/app/Http/Resources/SubCategoryResource.php
@@ -5,14 +5,14 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class SubCategoryResource extends JsonResource
+class DiscountResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
-            'category_id' => $this->category_id,
             'name' => $this->name,
+            'percentage' => $this->percentage,
             'description' => $this->description,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
